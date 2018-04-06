d3ca53a5d2cbb7fcde451e1ee584e9540cf07800
i868134 <i868134@c02vx60ghtd6.dhcp.slz.sap.corp>
2018-04-06 09:57:38 -0500

    initial commit to develop

--- TestProject/src/mttcc/TestProject.java
+++ TestProject/src/mttcc/TestProject.java
@@ -4,7 +4,7 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("test");
+        System.out.println("test develop");
 	}
 
 }
